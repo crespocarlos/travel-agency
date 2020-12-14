48389d5d121569e9b253c0b8dd2936b517fc47d7
Carlos Crespo <Carlos.Henrique.Crespo@adidas.com>
2020-12-14 22:53:02 +0100

    forecast app frontend done

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'weather-icons/css/weather-icons.min.css'
+import './index.css'
+import { Provider } from 'react-redux'
+import dotenv from 'dotenv'
+
+import store from './frontend/store'
+import App from './frontend/app'
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
+dotenv.config()
